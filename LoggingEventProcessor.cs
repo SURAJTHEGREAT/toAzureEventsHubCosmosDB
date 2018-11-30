b17938f9cf65fcccedf4269d38af370d937d839c
SURAJTHEGREAT <suraj.gate2013@gmail.com>
2018-11-30 13:04:36 +0000

    Add files via upload

---
+++ LoggingEventProcessor.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Azure.EventHubs;
+using Microsoft.Azure.EventHubs.Processor;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventHubProcessingNet
+{
+    class LoggingEventProcessor : IEventProcessor
+
+    {
+
+        public Task CloseAsync(PartitionContext context, CloseReason reason)
+        {
+            Console.WriteLine("LoggingEventProcessor closing, partition: " +
+                              $"'{context.PartitionId}', reason: '{reason}'.");
+            return Task.CompletedTask;
+        }
+
+        public Task OpenAsync(PartitionContext context)
+        {
+            Console.WriteLine("LoggingEventProcessor opened, processing partition: " +
+                              $"'{context.PartitionId}'");
+           
+            return Task.CompletedTask;
+        }
+
+        public Task ProcessErrorAsync(PartitionContext context, Exception error)
+        {
+            Console.WriteLine("LoggingEventProcessor error, partition: " +
+                              $"{context.PartitionId}, error: {error.Message}");
+            return Task.CompletedTask;
+        }
+
+        public  Task ProcessEventsAsync(PartitionContext context, IEnumerable<EventData> messages)
+        {
+            Console.WriteLine($"Batch of events received on partition '{context.PartitionId}'.");
+            foreach (var eventData in messages)
+            {
+                
+                var payload = Encoding.ASCII.GetString(eventData.Body.Array,
+                    eventData.Body.Offset,
+                    eventData.Body.Count);
+                
+                
+                //cosmos db can only send objects
+                var message = JsonConvert.DeserializeObject<MessageBody>(payload);
+                Console.WriteLine($"Payload sent to DB is '{payload}'.");
+
+                CosmosDB db = new CosmosDB();
+                db.Init().Wait();
+                db.pushTemperatureDocument(message).Wait();
+                //nullify created object
+                db = null;
+                Console.WriteLine("The object destroyed");
+               
+            }
+            return context.CheckpointAsync();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Azure.EventHubs;
+using Microsoft.Azure.EventHubs.Processor;
+using System;
+using System.Threading.Tasks;
+
+namespace EventHubProcessingNet
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            //take details from built-in endpoint
+            var eventHubName = "try-out-hub";
+            var iotHubConnectionString = "Endpoint=sb://iothub-ns-try-out-hu-1005128-51185d9614.servicebus.windows.net/;SharedAccessKeyName=iothubowner;SharedAccessKey=Z4VpahN/bHeRNaFp03ymXZU8L1ytMK3RD4gF3WWEbmE=;EntityPath=try-out-hub";
+
+            // take the connection string from storage created
+            var storageConnectionString = "DefaultEndpointsProtocol=https;AccountName=temperaturehubstorage;AccountKey=ZTcIUsl3/Iv+h6TouT1LtSSy76/vWfQar889GTGkhjRo1Z/mJbobA1+5rfZc3HYPglqIAG1qXwWBfulxSvLdcw==;EndpointSuffix=core.windows.net";
+            var storageContainerName = "device-to-cloud-events";
+
+            // imported from eventHub processor namespace
+            var consumerGroupName = PartitionReceiver.DefaultConsumerGroupName;
+
+            var processor = new EventProcessorHost(
+            eventHubName,
+            consumerGroupName,
+            iotHubConnectionString,
+            storageConnectionString,
+            storageContainerName);
+
+
+            
+            await processor.RegisterEventProcessorAsync<LoggingEventProcessor>();
+
+            Console.WriteLine("Event processor started, press enter to exit...");
+
+
+            Console.ReadLine();
+
+            await processor.UnregisterEventProcessorAsync();
+
+        }
+    }
+}
